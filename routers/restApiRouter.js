a3c7fba9d493d59582742ab65b8959d4573ad52b
Xu Lvcai <xulvcai2008@gmail.com>
2014-12-18 20:36:19 +0800

    update restapi router

--- routers/restApiRouter.js
+++ routers/restApiRouter.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 var authController = require('../controllers/oauth2server/authStrategys');
 
-router.route('/').get(authController.isBearerAuthenticated,
+router.route('/').get(
+  function(req, res) {
+    console.log("Cookies: ", req.cookies);
+    res.json({ message: 'You are reaching the REST API for fudan bbs!' });
+});
+
+router.route('/v1/').get(authController.isBearerAuthenticated,
   function(req, res) {
     console.log("Cookies: ", req.cookies);
     res.json({ message: 'You are reaching the REST API for fudan bbs!' });
