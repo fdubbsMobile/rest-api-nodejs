d8ffc9a1bcda7a1228a225390f81538b53a47b0c
Xu, Lvcai <xulvcai2008@gmail.com>
2014-12-29 21:17:28 +0800

    finish load posts

--- controllers/restApi/v1/postController.js
+++ controllers/restApi/v1/postController.js
@@ -26,64 +26,6 @@ function constructPost(rawData) {
 	return post;
 }
 
-function constructPostsUrl(loaded_by, board, 
-				organized_by, cursor, count) {
-	var url = config.bbs.host + "/bbs/";
-	if (organized_by == "TOPIC") {
-		url += "tdoc";
-	} else {
-		url += "doc";
-	}
-
-	if (loaded_by == "BNAME") {
-		url += "?board=";
-		url += board;
-	} else {
-		url += "?bid=";
-		url += board;
-	}
-
-	return url;
-}
-
-function constructPosts(rawData, reqCount, pageCount) {
-	var startIdx = pageCount - reqCount;
-	var count = 0;
-	for (var key in postsRaw) {
-		if (count < startIdx) {
-			continue;
-		}
-		count++;
-		var postMetadata = constructPostMetaData(postsRaw[key]);
-		if (!postMetadata) {
-			continue;
-		}
-		posts.push(postMetadata);
-	}
-}
-
-function constructPostMetaData(rawData) {
-	var title = rawData._;
-	var detail = rawData.$;
-
-	if (detail.sticky 
-		&& detail.sticky == "1") {
-		return null;
-	}
-
-	var metadata = {
-		id : detail.id,
-		title : title,
-		mark_sign : detail.m,
-		post_time : detail.time,
-		poster : {
-			name : detail.owner
-		}
-	};
-	return metadata;
-}
-
-
 function loadTopPosts (callback) {
 	var url = config.bbs.host + "/bbs/top10";
 	needle.get(url, options, function (error, response, body) {
@@ -113,14 +55,87 @@ function loadTopPosts (callback) {
 	});
 }
 
-function getPreviousCursor(total, current, count) {
-	if (current > total) {
+function constructPostsUrl(loaded_by, board, 
+				organized_by, cursor, count) {
+	var url = config.bbs.host + "/bbs/";
+	if (organized_by == "TOPIC") {
+		url += "tdoc";
+	} else {
+		url += "doc";
+	}
+
+	if (loaded_by == "BNAME") {
+		url += "?board=";
+		url += board;
+	} else {
+		url += "?bid=";
+		url += board;
+	}
+
+	return url;
+}
+
+function constructPosts(rawData, startIdx) {
+	var count = 0;
+	var posts = [];
+	for (var key in rawData) {
+		if (count < startIdx) {
+			count++;
+			continue;
+		}
+		var postMetadata = constructPostMetaData(rawData[key]);
+		if (!postMetadata) {
+			continue;
+		}
+		posts.push(postMetadata);
+	}
+
+	return posts;
+}
+
+function constructPostMetaData(rawData) {
+	var title = rawData._;
+	var detail = rawData.$;
+
+	if (detail.sticky 
+		&& detail.sticky == "1") {
+		return null;
+	}
+
+	var metadata = {
+		id : detail.id,
+		title : title,
+		mark_sign : detail.m,
+		post_time : detail.time,
+		poster : {
+			name : detail.owner
+		}
+	};
+	return metadata;
+}
+
+
+
+
+function getPreviousCursor(cursor, count, total) {
+	if (cursor >= total) {
 		return -1;
 	}
 
-	if (current + count > total) {
+	if (cursor + count >= total) {
 		return total;
 	}
+	return cursor + count;
+}
+
+function getNextCursor(cursor, count) {
+	if (cursor <=  0) {
+		return -1;
+	}
+	if (cursor - count <= 0) {
+		return 0;
+	}
+	return cursor - count;
 }
 
 function loadPosts (loaded_by, board, organized_by, cursor, count, callback) {
@@ -129,30 +144,34 @@ function loadPosts (loaded_by, board, organized_by, cursor, count, callback) {
 	needle.get(url, options, function (error, response, body) {
 		if (error) {
 			console.log("err "+ error);
-			callback(error, null);
+			return callback(error, null);
 		} else {
 			if (response.statusCode == 200) {
+				
 				var totalCount = parseInt(body.bbsdoc.brd.$.total);
 				var pageCount = parseInt(body.bbsdoc.brd.$.page);
 				var currentCursor = parseInt(body.bbsdoc.brd.$.start);
 
 				if (cursor > totalCount) {
 					return callback("Invalid cursor : " + cursor, null);
-				}				
-				var previous_cursor = 
+				}
+
+				var posts = constructPosts(body.bbsdoc.po, cursor - currentCursor);		
+				var previousCursor = getPreviousCursor(cursor == -1 ? currentCursor : cursor, 
+												posts.length, totalCount);
+				var nextCursor = getNextCursor(cursor == -1 ? currentCursor : cursor, count);
 				var result = {
 					count : posts.length,
-					previous_cursor : cursor + posts.length,
-					next_cursor : cursor - posts.length,
+					previous_cursor : previousCursor,
+					next_cursor : nextCursor,
 					post_list : posts
 				};
-
 				body.result = result;
-				callback(null, body);
+				return callback(null, body);
 			} else {
 				console.log("response status "+ response.statusCode);
 				console.log("response body "+ response.body);
-				callback(null, null);
+				return callback(null, null);
 			}
 		}
 	});
